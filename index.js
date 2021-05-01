db6ae285635d4d9852407e6fbc4563558ed31a8e
panse <47269934+panse98@users.noreply.github.com>
2021-05-01 02:11:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+
+res.send('hello world');
+
+});
+app.listen(3000,()=>console.log('listening on 3000'));
+
+
+app.post()
+app.put()
